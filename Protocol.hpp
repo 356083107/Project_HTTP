1c68c2291f65bb5a4b4c47e4593896851619bd05
Ding Zhenlu <96657228+356083107@users.noreply.github.com>
2022-09-03 23:08:18 +0800

    Add files via upload

--- Protocol.hpp
+++ Protocol.hpp
@@ -6,18 +6,59 @@
 #include<vector>
 #include<unordered_map>
 #include<string>
+#include<fcntl.h>
 #include<sys/types.h>
+#include<algorithm>
 #include<sys/stat.h>
 #include<sys/socket.h>
+#include <sys/sendfile.h>
 #include"Util.hpp"
 #include"Log.hpp"
 
 #define OK 200
 #define WEB_ROOT "wwwroot"
 #define HOME_PAGE "index.html"
+#define HTTP_VERSION "HTTP/1.0"
+#define LINE_END "\r\n"
 
 #define NOT_FOUND 404
 #define SEP ": "
+
+static std::string Code2Desc(int code)
+{
+   std::string desc;
+   switch(code)
+   {
+     case 200:desc ="OK";
+              break;
+     case 404:desc ="Not Found";
+              break;
+    default:break;
+
+   }
+   return desc;
+}
+
+static std::string Suffix2Desc(const std::string &suffix)
+
+{
+  
+  static std::unordered_map<std::string,std::string> suffix2desc ={
+  {".html","text/html"},
+  {".css","text/css"},
+  {".js","application/javascript"},
+  {".jpg","application/x-jpg"},
+};
+
+  auto iter =suffix2desc.find(suffix);
+  if(iter !=suffix2desc.end()){
+  return iter->second;
+}
+return "text/html";
+}
+
+
+
 
 class HttpRequest{
     
@@ -35,6 +76,7 @@ class HttpRequest{
       std::unordered_map<std::string,std::string>header_kv;
       int content_length;
       std::string path;
+      std::string suffix;
       std::string query_string;
 
       bool cgi;
@@ -54,8 +96,10 @@ class HttpResponse{
      std::string response_body;
 
      int status_code;
+     int fd;
+     int size;
   public:
-     HttpResponse():status_code(OK){}
+     HttpResponse():status_code(OK),fd(-1){}
 
     ~ HttpResponse(){}
 
@@ -101,6 +145,8 @@ class EndPoint{
     {  auto &line = http_request.request_line;
        std::stringstream ss(line);
        ss >> http_request.method >> http_request.uri >>http_request.version ;
+       auto &method =http_request.method;
+       std::transform(method.begin(),method.end(),method.begin(),::toupper);
        LOG(INFO,http_request.method);
        LOG(INFO,http_request.uri);
        LOG(INFO,http_request.version);
@@ -168,11 +214,44 @@ class EndPoint{
       ParseHttpRequestLine();
       ParseHttpRequestHeader();
     }
+
+int ProcessNonCgi(int size)
+    {
+      http_response.fd =open(http_request.path.c_str(),O_RDONLY);
+      if(http_response.fd >= 0){
+
+      http_response.status_line =HTTP_VERSION;
+      http_response.status_line +=" ";
+      http_response.status_line +=std::to_string(http_response.status_code);
+      http_response.status_line +=" ";
+      http_response.status_line +=Code2Desc(http_response.status_code);
+      http_response.status_line +=LINE_END;
+      http_response.size = size;
+      
+      std::string header_line= "Content-Length: " ;
+      header_line += std::to_string(size);
+      header_line +=LINE_END;
+      http_response.response_header.push_back(header_line);
+
+
+      header_line ="Content-Type: ";
+      header_line +=Suffix2Desc(http_request.suffix);
+      header_line +=LINE_END; 
+      http_response.response_header.push_back(header_line);
+
+        return OK;
+      }    
+        return 404;
+    }
+
     void BuildHttpResponse()
     {
       std::string _path;
       struct stat st;
+      int size =0;
+      std::size_t found =0;
       auto &code = http_response.status_code;
+
       if(http_request.method !="GET" && http_request.method !="POST"){
         //非法请求
         LOG(WARNING,"method is not right");
@@ -202,9 +281,8 @@ class EndPoint{
       if(http_request.path[http_request.path.size()-1] =='/'){
         http_request.path +=HOME_PAGE;
       }
-     // std::cout<<"debug: uri:"<<http_request.uri<<std::endl;
-     // std::cout<<"debug: path:"<<http_request.path<<std::endl;
-     // std::cout<<"debug: query_string:"<<http_request.query_string<<std::endl;
+    
+
       if(stat(http_request.path.c_str(),&st) == 0)
       {
         //说明资源是存在的
@@ -219,6 +297,7 @@ class EndPoint{
                     //特殊处理
                     http_request.cgi = true;
                 }
+                size =st.st_size;
               
 
       }
@@ -230,28 +309,46 @@ class EndPoint{
       code = NOT_FOUND;
       goto END;
     }
+    
+   found =http_request.path.rfind(".");
+   if(found == std::string::npos){
+      http_request.suffix =".html";
+   }
+   else{
+     http_request.suffix = http_request.path.substr(found);
+   }
+
     if(http_request.cgi){
       //ProcessCgi();
     }
     else{
-       ProcessNonCgi();    //返回静态网页
+      code = ProcessNonCgi(size);    //返回静态网页
+
       
     }
 
 
 END:
-      return ;
+    if(code != OK){
 
+      
     }
-   
-    int ProcessNonCgi()
-    {
-      return 0;
+
     }
 
     void SendHttpResponse()
     {
+      send(sock,http_response.status_line.c_str(),http_response.status_line.size(),0);
+      for(auto iter:http_response.response_header){
+        send(sock,iter.c_str(),iter.size(),0);
+      }
+     send(sock,http_response.blank.c_str(),http_response.blank.size(),0);
+     
+      
+     sendfile(sock, http_response.fd ,nullptr, http_response.size);
 
+    
+     close(http_response.fd);
     }
 
 
